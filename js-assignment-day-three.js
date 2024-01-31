fae081fb205a274c5400e1495b7c44dbfc44f87f
adityazanzane-coditas <158140097+adityazanzane-coditas@users.noreply.github.com>
2024-01-31 11:10:59 +0530

    Add files via upload

---
+++ js-assignment-day-three.js
@@ -0,0 +1,15 @@
+//Day-3 Assignment
+
+//Create a file and declare variables and assign string, boolean, undefined and null data types
+
+const fullName="Aditya Zanzane";//string
+const isMarried=false;//boolean
+const height=undefined;//undefined
+const salary=null;//null
+
+//Create file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof fullName);
+console.log(isMarried);
+console.log(height);
+console.log(salary);
