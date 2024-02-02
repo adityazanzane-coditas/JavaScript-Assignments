b0923665d3afeb3f8eb366af4ad38b09fd765089
adityazanzane-coditas <158140097+adityazanzane-coditas@users.noreply.github.com>
2024-02-02 10:59:39 +0530

    Update codewars-assignment-day-five.js

--- codewars-assignment-day-five.js
+++ codewars-assignment-day-five.js
@@ -1,3 +1,6 @@
+//CodeWars Assignment
+
+
 function feast(beast, dish) {
     if(beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length-1) == dish.charAt(dish.length-1)){
         return true;
