068e604104cf38fcbd6ed33fc9bb3be1c7c5f09a
Aditya Zanzane <aditya.zanzane@coditas.com>
2024-02-22 14:34:42 +0530

    this is updated file

--- js-assignment-day-17.js
+++ js-assignment-day-17.js
@@ -3,40 +3,25 @@
 const countriesAPI = "https://restcountries.com/v2/all";
 
 // Read the countries API using fetch and print the name of country, capital, languages, population and area.
-const countriesApiData = fetch(countriesAPI)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    data.forEach((country) => {
-      console.log("Name: ", country.name);
-      console.log("Capital: ", country.capital);
-      console.log("Languages: ");
-      country.languages.forEach((lang) => console.log(`   ${lang.name}`));
-      console.log("Population: ", country.population);
-      console.log("Area: ", Math.round(country.area / 10000));
+// Fetching countries data
+fetch(countriesAPI)
+  .then((response) => response.json())
+  .then((countries) => {
+    countries.forEach((country) => {
+      const { name, capital, languages, population, area } = country;
+      console.log(`Name: ${name}`);
+      console.log(`Capital: ${capital}`);
+      console.log(
+        `Languages: ${languages.map((language) => language.name).join(", ")}`
+      );
+      console.log(`Population: ${population}`);
+      console.log(`Area: ${area}`);
     });
+  })
+  .catch((error) => {
+    console.error("Error in fetching countries data:", error);
   });
 
-// Print out all the cat names in to catNames variable.
-// const catsAPI = "https://api.thecatapi.com/v1/breeds";
-
-// const catsName = new Promise((resolve, reject) => {
-//   fetch(catsAPI)
-//     .then((data) => resolve(data))
-//     .catch((error) => reject(error));
-// });
-
-// catsName
-//   .then((data) => {
-//     const cats = data.map(({ name }) => name);
-//     return cats;
-//   })
-//   .catch((error) => {
-//     console.log("Failed to fetch countries:", error);
-//   });
-
-// Read the cats api and find the average weight of cat in metric unit.
 // Read the countries api and find out the 10 largest countries
 fetch(countriesAPI)
   .then((response) => response.json())
@@ -59,10 +44,30 @@ fetch(countriesAPI)
   })
   .then((data) => {
     data.forEach((country) => {
-      country.languages.forEach((lang) => {
-        console.log(`   ${lang.name}`);
+      country.languages.forEach((language) => {
+        console.log(`${language.name}`);
         countOfLanguages++;
       });
       console.log("Count:", countOfLanguages);
     });
   });
+
+// Print out all the cat names in to catNames variable.
+// const catsAPI = "https://api.thecatapi.com/v1/breeds";
+
+// const catsName = new Promise((resolve, reject) => {
+//   fetch(catsAPI)
+//     .then((data) => resolve(data))
+//     .catch((error) => reject(error));
+// });
+
+// catsName
+//   .then((data) => {
+//     const cats = data.map(({ name }) => name);
+//     return cats;
+//   })
+//   .catch((error) => {
+//     console.log("Failed to fetch countries:", error);
+//   });
+
+// Read the cats api and find the average weight of cat in metric unit.
